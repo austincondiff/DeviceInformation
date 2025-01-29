0ae90ca261ac7c42d8e3d0af9c78681bc26f7384
Austin Condiff <austin.condiff@gmail.com>
2025-01-28 22:50:05 -0600

    Initial Commit

---
+++ DeviceInformation/DeviceInformationApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeviceInformationApp.swift
+//  DeviceInformation
+//
+//  Created by Austin Condiff on 1/28/25.
+//
+
+import SwiftUI
+
+@main
+struct DeviceInformationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
